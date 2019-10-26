73a5fc90bf2d819727f1b739ee188408315766bb
Diego Castillo <dcastillo@brainshark.com>
2019-10-25 19:24:12 -0500

    adding csharp8 project

--- CSharp8/Program.cs
+++ CSharp8/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace Interfaces
+namespace CSharp8
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Hello World!");
         }
     }
 }
